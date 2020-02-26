a4f4bf583c23cac2554976630a7e6964f5fc5b45
Antonin Décimo <antonin.decimo@gmail.com>
2020-02-26 17:42:33 +0100

    Windows build script.
    Currently has a bug in the compilation of cmdliner, a dependency of
    Opam.  See https://github.com/ocaml/opam/issues/4090.

--- windows/build.sh
+++ windows/build.sh
@@ -1,50 +1,66 @@
 #!/bin/sh
 
-set -eu
+# setup-x86_64 --root=C:\cygwin64 --quiet-mode --no-desktop --no-startmenu --packages=make,mingw64-x86_64-gcc-core,m4,patch --site "http://mirrors.kernel.org/sourceware/cygwin/"
+# C:\cygwin64\bin\mintty -
+# echo "none /cygdrive cygdrive noacl,binary,posix=0,user 0 0" > /etc/fstab
 
-PREFIX="C:/cygwin64/"
-BINDIR="${PREFIX}/bin"
-LIBDIR="${PREFIX}/lib/ocaml"
-OCAML_VERSION=4.10.0+rc2
-OCAML_PATH=ocaml-4.10.0-rc2
-FLEXDLL_VERSION=0.37
+OCAML_VERSION=4.09.0
+OCAML_PATH=ocaml-${OCAML_VERSION}
 OPAM_VERSION=2.0.6
+FLEXDLL_VERSION=0.37
+
+PREFIX=/usr/local
 BUILD=x86_64-pc-cygwin
 HOST=x86_64-w64-mingw32
 
-MAKEFLAGS="-j$(nproc)"
+BUILDDIR=/cygdrive/c/ocaml_platform_build
+
+MAKEFLAGS=-j$(nproc)
 export MAKEFLAGS
 
-mkdir -p "$PREFIX" "$LIBDIR"
+CYGWIN="nodosfilewarning winsymlinks:native"
+export CYGWIN
 
-curl -SLOfs "https://github.com/ocaml/ocaml/archive/${OCAML_VERSION}.tar.gz"
-curl -SLOfs "https://github.com/alainfrisch/flexdll/archive/${FLEXDLL_VERSION}.tar.gz"
-curl -SLOfs "https://github.com/ocaml/opam/archive/${OPAM_VERSION}.tar.gz"
+mkdir -p "$BUILDDIR"
+cd "$BUILDDIR" || exit 1
 
+curl -SLfs "https://github.com/ocaml/ocaml/archive/${OCAML_VERSION}.tar.gz" -o "${OCAML_VERSION}.tar.gz"
 tar xf "${OCAML_VERSION}.tar.gz"
-tar xf "${FLEXDLL_VERSION}.tar.gz"
-tar xf "${OPAM_VERSION}.tar.gz"
 
+curl -SLfs "https://github.com/alainfrisch/flexdll/archive/${FLEXDLL_VERSION}.tar.gz" -o "${FLEXDLL_VERSION}.tar.gz"
+tar xf "${FLEXDLL_VERSION}.tar.gz"
 mv "flexdll-${FLEXDLL_VERSION}"/* "${OCAML_PATH}/flexdll/"
 
-cd "${OCAML_PATH}" || exit
-
-# ln -s /usr/bin C:/cygwin64/usr/bin
-
-./configure --build=${BUILD} --host=${HOST} --prefix="$PREFIX" --bindir="$BINDIR" --libdir="${LIBDIR}"
+cd "${OCAML_PATH}" || exit 1
+./configure --build="$BUILD" --host="$HOST" --prefix="$PREFIX"
 make flexdll
 make world.opt
 make flexlink.opt
 make install
 
-OCAMLLIB=C:/cygwin64/lib/ocaml
+cd "$BUILDDIR" || exit 1
+
+curl -SLfs "https://github.com/ocaml/opam/releases/download/${OPAM_VERSION}/opam-full-${OPAM_VERSION}.tar.gz" -o "opam-full-${OPAM_VERSION}.tar.gz"
+tar xf "opam-full-${OPAM_VERSION}.tar.gz"
+
+OCAMLLIB=C:/cygwin64/usr/local/lib/ocaml
 CAML_LD_LIBRARY_PATH="$OCAMLLIB/stublibs;$OCAMLLIB"
 export OCAMLLIB
 export CAML_LD_LIBRARY_PATH
 
-cd "../opam-${OPAM_VERSION}" || exit
-
-./configure --build=$BUILD --host=$HOST --prefix="$PREFIX" --bindir="$BINDIR" --libdir="$LIBDIR"
-make lib-ext
-make
+cd "opam-full-${OPAM_VERSION}" || exit 1
+./configure --build="$BUILD" --host="$HOST" --prefix="$PREFIX"
+make lib-ext all -j1 OCAMLC='ocamlc -unsafe-string' OCAMLOPT='ocamlopt -unsafe-string'
 make install
+
+cd "$BUILDDIR" || exit 1
+
+OPAMYES=true
+export OPAMYES
+
+opam --init -a --disable-sandboxing
+eval $(opam env)
+opam switch create user ocaml-base-compiler.${OCAML_VERSION}
+opam switch remove default
+opam install depext
+opam depext -i $(cat packages)
