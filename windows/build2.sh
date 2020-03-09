e19253646986a64815d7bf906f8477704d89ab1f
Antonin Décimo <antonin.decimo@gmail.com>
2020-03-09 18:03:11 +0100

    Update windows build.

--- windows/build2.sh
+++ windows/build2.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Cygwin setup:
+#     setup-x86_64 --root=C:\cygwin64 --quiet-mode --no-desktop --no-startmenu --packages=curl,git,m4,make,mingw64-x86_64-gcc-core,nano,patch,rsync,unzip --site "http://mirrors.kernel.org/sourceware/cygwin/"
+#
+# MSYS2 MinGW-w64 setup:
+#     pacman -Syuu --noconfirm
+#     pacman -Syu --noconfirm
+#     pacman -S --needed --noconfirm base-devel mingw-w64-x86_64-toolchain curl git rsync unzip
+
 set -eu
 set -o xtrace
-
-# command -v curl >/dev/null 2>&1 || { echo >&2 "curl is missing." }
-# command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip is missing." }
-# command -v git >/dev/null 2>&1 || { echo >&2 "git is missing." }
-
 
 PREFIX=/opt/ocaml-platform
 OPAM_REPO='git://github.com/MisterDA/opam-repository.git#ocaml-platform'
@@ -17,20 +20,10 @@ DUNIVERSE_VERSION=depext
 DUNE_VERSION=2.3.1
 FLEXDLL_VERSION=0.37
 
-BUILD=x86_64-pc-msys
-HOST=x86_64-w64-mingw32
-
 BUILDDIR=/opt/ocaml-platform-build
-
-CURL='curl -SLfs'
-PACMAN='pacman -S --needed --noconfirm'
 
 # CYGWIN="nodosfilewarning winsymlinks:native"
 # export CYGWIN
-
-# Under MSYS2/MinGW-w64, flexlink adds wrong prefixes to library paths.
-FLEXLINKFLAGS='-L/usr/lib/w32api/ -L/mingw64/lib/gcc/x86_64-w64-mingw32/9.2.0/ -L/mingw64/lib/gcc/ -L/mingw64/x86_64-w64-mingw32/lib/x86_64-w64-mingw32/9.2.0/ -L/mingw64/x86_64-w64-mingw32/lib/ -L/mingw64/lib/x86_64-w64-mingw32/9.2.0/ -L/mingw64/lib/'
-export FLEXLINKFLAGS
 
 PATH="$PREFIX"/bin:"$PATH"
 export PATH
@@ -38,32 +31,54 @@ export PATH
 OPAMROOT="$PREFIX"/opam
 export OPAMROOT
 
+command -v curl >/dev/null 2>&1 || { echo >&2 "curl is missing."; }
+command -v git >/dev/null 2>&1 || { echo >&2 "git is missing."; }
+command -v make >/dev/null 2>&1 || { echo >&2 "make is missing."; }
+command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip is missing."; }
 
-$PACMAN base-devel curl git unzip
-$PACMAN mingw-w64-x86_64-toolchain # msys2-devel
+case "$(uname)" in
+    CYGWIN*)
+        BUILD=x86_64-pc-cygwin
+        HOST=x86_64-w64-mingw32
+        ;;
+    MSYS*) # MSYS2+MinGW-w64
+        BUILD=x86_64-pc-msys
+        HOST=x86_64-w64-mingw32
 
-# Work around a MSYS2 bug. The two utilities are not correctly prefixed.
-# https://github.com/msys2/MSYS2-packages/issues/937
-if [ ! -e /mingw64/bin/x86_64-w64-mingw32-windres.exe ]; then
-    ln -s /mingw64/bin/windres.exe /mingw64/bin/x86_64-w64-mingw32-windres.exe
-fi
-if [ ! -e /mingw64/bin/x86_64-w64-mingw32-as.exe ]; then
-    ln -s /mingw64/bin/as.exe /mingw64/bin/x86_64-w64-mingw32-as.exe
-fi
+        # Flexlink adds wrong prefixes to library paths.
+        FLEXLINKFLAGS='-L/usr/lib/w32api/ -L/mingw64/lib/gcc/x86_64-w64-mingw32/9.2.0/ -L/mingw64/lib/gcc/ -L/mingw64/x86_64-w64-mingw32/lib/x86_64-w64-mingw32/9.2.0/ -L/mingw64/x86_64-w64-mingw32/lib/ -L/mingw64/lib/x86_64-w64-mingw32/9.2.0/ -L/mingw64/lib/'
+        export FLEXLINKFLAGS
+
+        # Work around a MSYS2 bug. The two utilities are not correctly prefixed.
+        # https://github.com/msys2/MSYS2-packages/issues/937
+        PATH=/mingw64/bin:$PATH
+        export PATH
+        MSYS_PREFIX=/mingw64
+        if [ ! -e "${MSYS_PREFIX}/bin/${HOST}-windres.exe" ]; then
+            ln -s "${MSYS_PREFIX}"/bin/windres.exe "${MSYS_PREFIX}/bin/${HOST}-windres.exe"
+        fi
+        if [ ! -e "${MSYS_PREFIX}/bin/${HOST}-as.exe" ]; then
+            ln -s "${MSYS_PREFIX}"/bin/as.exe "${MSYS_PREFIX}/bin/${HOST}-as.exe"
+        fi
+
+        ;;
+    *)
+        echo >&2 "Host not supported"
+        exit 1
+esac
 
 mkdir -p "$PREFIX" "$BUILDDIR"
 cd "$BUILDDIR" || exit
 
-$CURL "https://github.com/ocaml/ocaml/archive/${OCAML_VERSION}.tar.gz" -o ocaml.tar.gz
+curl -SLfs "https://github.com/ocaml/ocaml/archive/${OCAML_VERSION}.tar.gz" -o ocaml.tar.gz
 tar xf ocaml.tar.gz
 
-$CURL "https://github.com/alainfrisch/flexdll/archive/${FLEXDLL_VERSION}.tar.gz" -o flexdll.tar.gz
+curl -SLfs "https://github.com/alainfrisch/flexdll/archive/${FLEXDLL_VERSION}.tar.gz" -o flexdll.tar.gz
 tar xf flexdll.tar.gz
 
 # Allow C++ compilation
-$CURL "https://github.com/alainfrisch/flexdll/pull/48.diff" -o 48.diff
-# For some reason, the patch is only applicable with Git.
-git -C flexdll-${FLEXDLL_VERSION} apply ../48.diff
+curl -SLfs "https://github.com/alainfrisch/flexdll/pull/48.diff" -o 48.diff
+patch -d flexdll-${FLEXDLL_VERSION} -p1 < 48.diff
 cp -r flexdll-${FLEXDLL_VERSION}/* ocaml-${OCAML_VERSION}/flexdll/
 
 cd ocaml-$OCAML_VERSION || exit
@@ -76,8 +91,13 @@ make -j"$(nproc)" install
 cd "$BUILDDIR" || exit
 # $CURL "https://github.com/ocaml/opam/releases/download/${OPAM_VERSION}/opam-full-${OPAM_VERSION}.tar.gz" -o opam-full.tar.gz
 # tar xf opam-full.tar.gz
-$CURL "https://github.com/ocaml/opam/archive/master.zip" -o opam.zip
+curl -SLfs "https://github.com/ocaml/opam/archive/master.zip" -o opam.zip
 unzip opam.zip
+
+OCAMLLIB=/opt/ocaml-platform/lib/ocaml
+export OCAMLLIB
+CAML_LD_LIBRARY_PATH=$OCAMLLIB/stublibs:$OCAMLLIB
+export CAML_LD_LIBRARY_PATH
 
 # cd "opam-full-${OPAM_VERSION}" || exit
 cd opam-master || exit
@@ -88,8 +108,7 @@ sed -e 's|^URL_mccs.*|URL_mccs = https://github.com/AltGr/ocaml-mccs/archive/1.1
 make lib-ext all -j1 DUNE_ARGS='--verbose ' #  OCAMLC='ocamlc -unsafe-string' OCAMLOPT='ocamlopt -unsafe-string'
 make install
 
-
-cd "$PREFIX" | exit
+cd "$PREFIX" || exit
 opam init -a --disable-sandboxing -y $OPAM_REPO
 eval $(opam env)
 opam install -y depext
