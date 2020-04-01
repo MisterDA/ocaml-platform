02a32d1ae0de1e93e16100606e6dd373ccdacea3
Antonin Décimo <antonin.decimo@gmail.com>
2020-04-02 00:20:30 +0200

    Unifiy Windows build scripts.

---
+++ windows/build.sh
@@ -0,0 +1,186 @@
+#!/bin/sh
+
+set -eu
+set -o xtrace
+
+# Execute one of the subsections below in Windows to prepare the build
+# environment.
+#
+# 1. MSVC
+# 1.1. Cygwin
+#
+# Run in x64 Native Tools Command Prompt for VS.
+#
+#     setup-x86_64 --root=C:\cygwin64 --quiet-mode --no-desktop --no-startmenu ^
+#                  --packages=curl,diffutils,git,m4,make,nano,patch,rsync,unzip ^
+#                  --site "http://mirrors.kernel.org/sourceware/cygwin/"
+#     C:\cygwin64\bin\mintty -
+#
+# 2. MinGW-w64
+# 2.1. Cygwin
+#
+#     setup-x86_64 --root=C:\cygwin64 --quiet-mode --no-desktop --no-startmenu ^
+#                  --packages=curl,git,m4,make,mingw64-x86_64-gcc-core,nano,patch,rsync,unzip ^
+#                  --site "http://mirrors.kernel.org/sourceware/cygwin/"
+#     C:\cygwin64\bin\mintty -
+#
+# 2.2. MSYS2
+#
+# Run in the MSYS2 shell.
+#
+#     pacman -Syuu --noconfirm
+#     pacman -Syu --noconfirm
+#     pacman -S --needed --noconfirm base-devel mingw-w64-cross-toolchain curl git rsync unzip
+#
+#
+# Then, run in the last opened shell
+#
+#     ./build.sh -c msvc        # if compiling with MSVC
+#     ./build.sh -c mingw       # if compiling with MinGW-w64
+#
+
+OPAM_REPO='git://github.com/MisterDA/opam-repository.git#ocaml-platform'
+
+OCAML_VERSION=4.10.0
+OPAM_VERSION=master
+DUNIVERSE_VERSION=master
+FLEXDLL_VERSION=0.37
+
+
+
+command -v curl >/dev/null 2>&1 || { echo >&2 "curl is missing."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "git is missing."; exit 1; }
+command -v make >/dev/null 2>&1 || { echo >&2 "make is missing."; exit 1; }
+command -v patch >/dev/null 2>&1 || { echo >&2 "patch is missing."; exit 1; }
+command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip is missing.";  exit 1; }
+
+
+while getopts 'c:' c; do
+    case $c in
+        c)
+            case $OPTARG in
+                msvc) COMPILER=$OPTARG ;;
+                mingw) COMPILER=$OPTARG ;;
+                *)  echo >&2 "Unsupported '$c' compiler."; exit 1 ;;
+            esac ;;
+        *)  echo >&2 "Unsupported '$c' option."; exit 1 ;;
+    esac
+done
+
+if [ -z "${COMPILER-}" ]; then
+    echo >&2 "Must set a compiler with -c <msvc|mingw>."
+    exit 1
+fi
+
+
+case $COMPILER in
+    msvc)
+        BUILD=x86_64-unknown-cygwin
+        HOST=x86_64-pc-windows
+
+        PREFIX="${PROGRAMFILES}\\Ocaml Platform"
+        PATH="$(cygpath "${PREFIX}\\bin"):${PATH}"
+        export PATH
+        OPAMROOT="${PREFIX}\\opam"
+        export OPAMROOT
+        OCAMLLIB="${PREFIX}\\lib\\ocaml"
+        export OCAMLLIB
+        CAML_LD_LIBRARY_PATH="${OCAMLLIB}\\stublibs;${OCAMLLIB}"
+        export CAML_LD_LIBRARY_PATH
+        ;;
+    mingw)
+        PREFIX=/opt/ocaml-platform
+        PATH="${PREFIX}/bin:${PATH}"
+        export PATH
+        OPAMROOT="${PREFIX}/opam"
+        export OPAMROOT
+
+        case "$(uname)" in
+            CYGWIN_NT*)
+                BUILD=x86_64-pc-cygwin
+                HOST=x86_64-w64-mingw32
+                ;;
+            MSYS*)
+                BUILD=x86_64-pc-msys
+                HOST=x86_64-w64-mingw32
+                ;;
+            MINGW64*)
+                BUILD=x86_64-pc-msys
+                HOST=x86_64-w64-mingw32
+
+                OCAMLLIB="C:\\${MSYSTEM_PREFIX}\\opt\\ocaml-platform\\lib\\ocaml"
+                export OCAMLLIB
+                CAML_LD_LIBRARY_PATH="${OCAMLLIB}\\stublibs;${OCAMLLIB}"
+                export CAML_LD_LIBRARY_PATH
+
+                # Flexlink adds wrong prefix to library paths.
+                FLEXLINKFLAGS=$(cat <<EOF
+-L/usr/lib/w32api/
+-L$MSYSTEM_PREFIX/lib/gcc/x86_64-w64-mingw32/9.2.0/
+-L$MSYSTEM_PREFIX/lib/gcc/
+-L$MSYSTEM_PREFIX/x86_64-w64-mingw32/lib/x86_64-w64-mingw32/9.2.0/
+-L$MSYSTEM_PREFIX/x86_64-w64-mingw32/lib/
+-L$MSYSTEM_PREFIX/lib/x86_64-w64-mingw32/9.2.0/
+-L$MSYSTEM_PREFIX/lib/
+EOF
+                )
+                export FLEXLINKFLAGS
+                ;;
+            *) echo >&2 "Unsupported '$(uname)' build environment."; exit 1 ;;
+        esac
+esac
+
+
+mkdir -p "$(cygpath "$PREFIX")"
+
+
+curl -SLfs "https://github.com/ocaml/ocaml/archive/${OCAML_VERSION}.tar.gz" \
+     -o "ocaml-${OCAML_VERSION}.tar.gz"
+tar xf "ocaml-${OCAML_VERSION}.tar.gz"
+
+curl -SLfs "https://github.com/alainfrisch/flexdll/archive/${FLEXDLL_VERSION}.tar.gz" \
+     -o "flexdll-${FLEXDLL_VERSION}.tar.gz"
+tar xf "flexdll-${FLEXDLL_VERSION}.tar.gz"
+
+
+# Allow C++ compilation
+curl -SLfs "https://github.com/alainfrisch/flexdll/pull/48.diff" -o 48.diff
+patch -d "flexdll-${FLEXDLL_VERSION}" -p1 < 48.diff
+cp -r "flexdll-${FLEXDLL_VERSION}"/* "ocaml-${OCAML_VERSION}/flexdll/"
+
+
+cd ocaml-$OCAML_VERSION || exit
+if [ "${COMPILER}" = msvc ]; then
+    eval $(tools/msvs-promote-path)
+fi
+./configure --build="$BUILD" --host="$HOST" --prefix="$(cygpath -m "$PREFIX")"
+make -j"$(nproc)" flexdll V=1
+make -j"$(nproc)" world.opt V=1
+make -j"$(nproc)" flexlink.opt V=1
+make -j"$(nproc)" install
+
+
+cd .. || exit
+curl -SLfs "https://github.com/ocaml/opam/archive/${OPAM_VERSION}.zip" \
+     -o "opam-${OPAM_VERSION}.zip"
+unzip opam-$OPAM_VERSION.zip
+cd opam-$OPAM_VERSION
+
+sed -E -i src_ext/Makefile.sources \
+    -e 's|^(URL_dune-local = ).*$|\1https://github.com/ocaml/dune/archive/1.11.4.tar.gz|' \
+    -e 's|^(MD5_dune-local = ).*$|\13c04b502b0b17d60b805b730eefe61a6|'
+mkdir -p src_ext/patches/dune-local
+curl -SLfs 'https://github.com/MisterDA/dune/commit/261ff273e099d73ea5023d82b83cf10390feb7bd.patch' \
+     -o src_ext/patches/dune-local/0001-Quote-program-path-given-to-exec.patch
+
+./configure --build="$BUILD" --host="$HOST" --prefix="$(cygpath -m "$PREFIX")"
+make lib-ext all -j1 DUNE_ARGS='--verbose' V=1
+make install
+
+
+cd "$PREFIX" || exit
+opam init -a --disable-sandboxing -y "$OPAM_REPO"
+eval $(opam env)
+opam install -y --with-doc \
+    $(opam list --required-by ocaml-platform --columns=package -s) \
+    ocaml-platform
