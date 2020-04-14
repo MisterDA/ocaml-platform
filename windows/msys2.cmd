6599e24cc1e23208573ff8afb47183b7216c9dc9
Antonin Décimo <antonin.decimo@gmail.com>
2020-04-14 17:04:21 +0200

    Set MSYS2 packages.

--- windows/msys2.cmd
+++ windows/msys2.cmd
@@ -2,17 +2,19 @@
 set CHERE_INVOKING=1
 
 if "%HOST%" equ "pc-windows" (
+  set MSYS2_PACKAGES=base-devel git unzip
   call :UpgradeMSYS2
   set MSYSTEM=MSYS
 )
 
 if "%HOST%" equ "pc-msys" (
+  set MSYS2_PACKAGES=base-devel git msys2-devel unzip
   call :UpgradeMSYS2
   set MSYSTEM=MSYS
 )
 
 if "%HOST%" equ "w64-mingw32" (
-  set MSYS2_PACKAGES=base-devel curl git mingw-w64-toolchain rsync unzip
+  set MSYS2_PACKAGES=base-devel git mingw-w64-toolchain unzip
   call :UpgradeMSYS2
   set MSYSTEM=MINGW64
 )
